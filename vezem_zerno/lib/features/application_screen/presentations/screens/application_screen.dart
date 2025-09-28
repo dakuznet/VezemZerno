5a88ee151bd586a1e7b9298220476eafc328c954
DimastaK05 <karpun45@gmail.com>
2025-09-29 00:58:29 +0300

    update style

--- vezem_zerno/lib/features/application_screen/presentations/screens/application_screen.dart
+++ vezem_zerno/lib/features/application_screen/presentations/screens/application_screen.dart
@@ -60,10 +60,21 @@ class _ApplicationScreenState extends State<ApplicationScreen>  with SingleTicke
         elevation: 0,
         title: Text(
           'Заявки', 
-          style: TextStyle(fontSize: 18.sp, color: ColorsConstants.primaryBrownColor,),
+          style: TextStyle(fontSize: 16.sp, color: ColorsConstants.primaryBrownColor, fontFamily: 'Unbounded',fontWeight: FontWeight.w500,),
           ),
           centerTitle: true,
-          bottom: TabBar(controller: _tabController, tabs: [Tab(text: 'Все заявки',), Tab(text: 'С моим откликом',)],          labelColor: Colors.black,
+          bottom: TabBar(
+            controller: _tabController, 
+            tabs: [
+              Tab(text: 'Все заявки',), 
+              Tab(text: 'С моим откликом',)
+              ], 
+              labelColor: ColorsConstants.primaryBrownColor, 
+              labelStyle: TextStyle(
+                fontSize: 14.sp, 
+                fontFamily: 'Unbounded',
+                fontWeight: FontWeight.w500,
+                color: ColorsConstants.primaryBrownColor),
           indicatorColor: const Color.fromARGB(255, 0, 0, 0),
   ),
       ),
@@ -128,28 +139,35 @@ class ApplicationCard extends StatelessWidget {
               application.number,
               style: TextStyle(
                 fontSize: 16.sp,
-                fontWeight: FontWeight.bold,
-                color:  ColorsConstants.primaryBrownColor
+                fontWeight: FontWeight.w500,
+                color:  ColorsConstants.primaryBrownColor,
+                fontFamily: "Unbounded"
               ),
             ),
-            SizedBox(height: 8.h),
             // Дата и статус
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(application.date, style: TextStyle(fontWeight: FontWeight.normal,color: ColorsConstants.primaryBrownColorWithOpacity,),),
+                Text(application.date, 
+                style: TextStyle(
+                  fontFamily: 'Unbounded',
+                  fontWeight: FontWeight.w300,
+                  fontSize: 12.sp,
+                  color: ColorsConstants.primaryBrownColorWithOpacity,
+                  ),
+                ),
                 Text(
                   application.status,
                   style: TextStyle(
+                    fontFamily: 'Unbounded',
+                    fontWeight: FontWeight.w300,
+                    fontSize: 12.sp,
                     color: ColorsConstants.primaryBrownColor,
-                    fontWeight: FontWeight.normal,
                   ),
                 ),
               ],
             ),
-            SizedBox(height: 16.h),
             
-            // Разделитель
             Divider( height: 16,
       thickness: 1,
       color: ColorsConstants.primaryBrownColor),
@@ -159,44 +177,51 @@ class ApplicationCard extends StatelessWidget {
            Column(
   crossAxisAlignment: CrossAxisAlignment.start,
   children: [
-    // Строка "Откуда"
     Text.rich(
       TextSpan(
-        text: 'Откуда: ',
+        text: 'Откуда:  ',
         style: TextStyle(
-          fontWeight: FontWeight.normal, 
-          color: ColorsConstants.primaryBrownColorWithOpacity
+          fontFamily: 'Unbounded',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 12.sp,
+                      color: ColorsConstants.primaryBrownColor,
         ),
         children: [
           TextSpan(
             text: application.from,
             style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: ColorsConstants.primaryBrownColor
+              fontFamily: 'Unbounded',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14.sp,
+                      color: ColorsConstants.primaryBrownColor,
             ),
           ),
         ],
       ),
     ),
     
-    // Добавляем отступ между строками
+
     SizedBox(height: 8),
     
-    // Строка "Куда" (если есть значение)
+
     if (application.to.isNotEmpty)
       Text.rich(
         TextSpan(
-          text: 'Куда: ',
+          text: 'Куда:  ',
           style: TextStyle(
-            fontWeight: FontWeight.normal, 
-            color: ColorsConstants.primaryBrownColorWithOpacity
+            fontFamily: 'Unbounded',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 12.sp,
+                      color: ColorsConstants.primaryBrownColor,
           ),
           children: [
             TextSpan(
               text: application.to,
               style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: ColorsConstants.primaryBrownColor
+                fontFamily: 'Unbounded',
+                      fontWeight: FontWeight.w600,
+                      fontSize: 14.sp,
+                      color: ColorsConstants.primaryBrownColor,
               ),
             ),
           ],
@@ -205,30 +230,82 @@ class ApplicationCard extends StatelessWidget {
   ],
 ),
             SizedBox(height: 16.h),
-            
             // Информация о грузе
             if (application.cargo.isNotEmpty)
-              Text.rich(
-                TextSpan(
-                  text: application.cargo,
-                  style: TextStyle(fontWeight: FontWeight.bold, color: ColorsConstants.primaryBrownColor),
-                  children: [
-                    if (application.weight.isNotEmpty)
-                      TextSpan(
-                        text: ' ${application.weight}',
-                        style: TextStyle(fontWeight: FontWeight.bold, color: ColorsConstants.primaryBrownColor),
-                      ),
-                    if (application.size.isNotEmpty)
-                      TextSpan(
-                        text: ' ${application.size}',
-                        style: TextStyle(fontWeight: FontWeight.bold, color: ColorsConstants.primaryBrownColor),
-                      ),
-                  ],
-                ),
-              ),
+              Row(
+  mainAxisAlignment: MainAxisAlignment.center,
+  children: [
+    // Овал 1
+    Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: ColorsConstants.primaryBrownColor,
+          width: 2.0,
+        ),
+        borderRadius: BorderRadius.circular(50),
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+      margin: EdgeInsets.only(right: 8), // Отступ справа
+      child: Text(
+        application.cargo,
+        style: TextStyle(
+          fontSize: 11.sp,
+          fontFamily: 'Unbounded',
+          fontWeight: FontWeight.bold, 
+          color: ColorsConstants.primaryBrownColor,
+        ),
+      ),
+    ),
+    
+    // Овал 2
+    if (application.weight.isNotEmpty)
+      Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: ColorsConstants.primaryBrownColor,
+            width: 2.0,
+          ),
+          borderRadius: BorderRadius.circular(50),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+        margin: EdgeInsets.symmetric(horizontal: 8), // Отступы слева и справа
+        child: Text(
+          application.weight,
+          style: TextStyle(
+            fontSize: 11.sp,
+            fontFamily: 'Unbounded',
+            fontWeight: FontWeight.bold, 
+            color: ColorsConstants.primaryBrownColor,
+          ),
+        ),
+      ),
+    
+    // Овал 3
+    if (application.size.isNotEmpty)
+      Container(
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: ColorsConstants.primaryBrownColor,
+            width: 2.0,
+          ),
+          borderRadius: BorderRadius.circular(50),
+        ),
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+        margin: EdgeInsets.only(left: 8), // Отступ слева
+        child: Text(
+          application.size,
+          style: TextStyle(
+            fontSize: 11.sp,
+            fontFamily: 'Unbounded',
+            fontWeight: FontWeight.bold, 
+            color: ColorsConstants.primaryBrownColor,
+          ),
+        ),
+      ),
+  ],
+),
             SizedBox(height: 16.h),
             
-            // Разделитель
             Divider( height: 16,
           thickness: 1,
           color: ColorsConstants.primaryBrownColor),
@@ -242,7 +319,13 @@ class ApplicationCard extends StatelessWidget {
                   backgroundColor: ColorsConstants.primaryButtonBackgroundColor
                 ),
                 onPressed: () {},
-                child: Text('Откликнуться', style: TextStyle(color: ColorsConstants.primaryBrownColor,fontWeight: FontWeight.bold ),),
+                child: Text(
+                  'Откликнуться', 
+                  style: TextStyle(
+                    fontFamily: 'Unbounded',
+                    fontSize: 16.sp,
+                    fontWeight: FontWeight.w500,
+                    color: ColorsConstants.primaryBrownColor,),),
               ),
             ),
           ],
